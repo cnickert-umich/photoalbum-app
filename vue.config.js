259031bfecc447a672543a79315ca022e697b90b
Cameron M Nickert <cameron3pointer@gmail.com>
2019-12-13 22:59:49 -0500

    Reverted

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-{
-    chainWebpack: config => {
-        config.plugin('copy').tap(([options]) => {
-            options[0].ignore = ['./index.html', '.DS_Store']
-            return [options]
-        })
-    }
-}
